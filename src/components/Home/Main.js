341cfbe08a59b6937563d07614f4a3b7bc0b528b
Simple-temp <fiveriam01@gmail..com>
2022-07-25 22:26:10 +0600

    adding elastic carousel

--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -1,7 +1,16 @@
 import { useQuery } from '@apollo/client';
 import React from 'react';
-import { Carousel, Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { GET_USERS, GET_FOODS, GET_ORDERS, GET_USER_BY_ID, GET_FOOD_BY_ID, GET_ORDER_BY_ID } from '../../gqloperation/Queres';
+import Carousel from "react-elastic-carousel";
+import Item from '../Item';
+
+const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 },
+];
 
 const Main = () => {
 
@@ -28,8 +37,17 @@ const Main = () => {
 
 
     return (
-        <Container>
-            slider
+        <Container className='App'>
+            <Carousel breakPoints={breakPoints}>
+                <Item>One</Item>
+                <Item>Two</Item>
+                <Item>Three</Item>
+                <Item>Four</Item>
+                <Item>Five</Item>
+                <Item>Six</Item>
+                <Item>Seven</Item>
+                <Item>Eight</Item>
+            </Carousel>
         </Container>
     );
 };
--- src/components/Home/Recipes.js
+++ src/components/Home/Recipes.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Carousel from "react-elastic-carousel";
+import Item from '../Item';
+
+const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 },
+];
 
 const Recipes = () => {
     return (
-        <div>
-            recipes
-        </div>
+        <Container className='App'>
+            <Carousel breakPoints={breakPoints}>
+                <Item>One</Item>
+                <Item>Two</Item>
+                <Item>Three</Item>
+                <Item>Four</Item>
+                <Item>Five</Item>
+                <Item>Six</Item>
+                <Item>Seven</Item>
+                <Item>Eight</Item>
+            </Carousel>
+        </Container>
     );
 };
 
