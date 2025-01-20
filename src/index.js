734aaf98278f7de3c2c31d0a21faf312dd70bc12
Simple-temp <reingsroman042@gmail.com>
2025-01-20 14:28:38 +0600

    change backend url

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import store from './redux/Store';
 const info = JSON.parse(localStorage.getItem("userInfo"))
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'https://resturant-7fgl.onrender.com/graphql',
   cache: new InMemoryCache(),
   headers : {
     authorization : info ? `Bearer ${info.token}` : ""
